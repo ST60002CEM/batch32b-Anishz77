0396c3316959a7d3c14fb9db00866285610a905b
Anishz77 <pantaanish77@gmail.com>
2024-05-16 13:58:16 +0545

    push thimport 'package:flutter/material.dart';

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,6 @@
 import 'package:final_project/screen/login_screen.dart';
+import 'package:final_project/screen/signup_screen.dart';
+import 'package:final_project/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -10,7 +12,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: SplashScreen(),
     );
   }
 }
--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,11 +1,52 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
+import 'package:final_project/screen/signup_screen.dart';
+import 'package:flutter/material.dart';
+ 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+ 
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Add a delay to simulate the splash screen duration
+    Future.delayed(Duration(seconds: 3), () {
+      // Navigate to the sign-up screen after the delay
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => SignUpScreen()),
+      );
+    });
+  }
+ 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Display your custom image logo using Image.asset
+            Image.asset(
+              'assets/images/trekking.webp', // Replace 'assets/logo.png' with your image file path
+              width: 300, // Adjust width as needed
+              height: 300, // Adjust height as needed
+            ),
+            SizedBox(height: 10), // Add some space between the image and text
+            // Text "since 2024" aligned to the right-hand side
+            Text(
+              'Trail Trekker',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+ 
