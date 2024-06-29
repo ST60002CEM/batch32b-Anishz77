b8f5126c440efa51db2e377356d2eaacde1d4364
Anishz77 <pantaanish77@gmail.com>
2024-06-29 13:20:58 +0545

    dashboard model

---
+++ lib/features/dashboard/presentation/viewmodel/dashboard_view_model.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class DashboardViewModel extends ChangeNotifier {
+  // Example data, replace with your own logic
+  List<Map<String, String>> featuredProducts = [
+    {'name': 'SideBag', 'price': '\Rs 2400', 'image': 'assets/image/sidebag.webp'},
+    {'name': 'PeakPack', 'price': '\Rs 3000', 'image': 'assets/image/peakpack.webp'},
+    {'name': 'Hikie', 'price': '\Rs1800', 'image': 'assets/image/Hikie.webp'},
+  ];
+
+  List<Map<String, String>> popularProducts = [
+    {'name': 'CasualBag', 'price': '\$2400', 'image': 'assets/image/sidebag.webp'},
+    {'name': 'Wiry', 'price': '\Rs2700', 'image': 'assets/image/wiry.webp'},
+    {'name': 'Rodless', 'price': '\Rs1900', 'image': 'assets/image/rodless.webp'},
+  ];
